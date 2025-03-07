0ee1e339740c435580adba9ad248a2c96b864909
marco-gulino <marcogulino98@gmail.com>
2025-03-07 17:04:13 +0100

    [feat] set destroy CRUD function in postController

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -6,11 +6,12 @@ function index(req, res) {
   // QUERY
   const showPosts = `SELECT * FROM posts`;
 
-  // Execute QUERY
+  // Execute QUERY that prints every Post
   connection.query(showPosts, (err, results) => {
     if (err) return res.status(500).json({ error: "Database query failed" });
     if (results.length === 0)
       return res.status(404).json({ error: "Posts List is Empty" });
+
     // Send RES
     res.json(results);
   });
@@ -29,7 +30,22 @@ function update(req, res) {}
 function modify(req, res) {}
 
 // DESTROY
-function destroy(req, res) {}
+function destroy(req, res) {
+  // GET ID from URL
+  const id = req.params.id;
+  // QUERY
+  const destroyPost = `DELETE FROM posts WHERE id = ?`;
+
+  // Execute QUERY that removes a post using the id
+  connection.query(destroyPost, [id], (err, results) => {
+    if (err) return res.status(500).json({ error: "Database query failed" });
+    if (!results.affectedRows)
+      return res.status(404).json({ error: "Post not found" });
+
+    //RETURN STATUS
+    res.sendStatus(204);
+  });
+}
 
 // EXPORT Functions
 module.exports = { index, show, store, update, modify, destroy };
